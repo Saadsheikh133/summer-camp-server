3affe310eab48255ca6f2f63f0d199c34401bb17
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-06-09 23:05:28 +0600

    implement jwt

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access!!!" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access!!!" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vuuhbip.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,44 +47,59 @@ async function run() {
 
     const sportsCollection = client.db("sportsDB").collection("classes");
     const allClassesCollection = client.db("sportsDB").collection("allClasses");
-    const selectedClassCollection = client.db("sportsDB").collection("selectedClass");
+    const selectedClassCollection = client
+      .db("sportsDB")
+      .collection("selectedClass");
+    const usersCollection = client.db("sportsDB").collection("users");
     const instructorsCollection = client
       .db("sportsDB")
       .collection("instructor");
 
     // sports class related api
     app.get("/classes", async (req, res) => {
-      const result = await sportsCollection.find().sort({ students: -1}).toArray();
+      const result = await sportsCollection
+        .find()
+        .sort({ students: -1 })
+        .toArray();
       res.send(result);
     });
 
     // instructor related api
-    app.get('/instructors', async (req, res) => {
+    app.get("/instructors", async (req, res) => {
       const result = await instructorsCollection
         .find()
-        .sort({ Number_of_Courses : -1})
+        .sort({ Number_of_Courses: -1 })
         .toArray();
       res.send(result);
     });
 
     // allClasses related api
-    app.get('/allClasses', async (req, res) => {
+    app.get("/allClasses", async (req, res) => {
       const result = await allClassesCollection.find().toArray();
       res.send(result);
     });
 
-
     app.get("/selectedClass", async (req, res) => {
       const result = await selectedClassCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.post('/addToCarts', async (req, res) => {
+    app.post("/addToCarts", async (req, res) => {
       const classes = req.body;
       const result = await selectedClassCollection.insertOne(classes);
       res.send(result);
-    })
+    });
 
+    app.post("/addUsers", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
